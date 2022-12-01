75247d9b00bda9f128bec5cc08563e1cb426e8cb
Owen Young <theowenyoung@gmail.com>
2022-12-01 19:33:33 +0800

    fix: ci

--- scripts/install_mdbook.sh
+++ scripts/install_mdbook.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
+set -e
 
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 BIN_DIR="$SCRIPT_DIR/../bin"
-binname="mdbook-epub"
 mkdir -p $BIN_DIR
+OSTYPE="linux-gnu-xx"
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     cd -- /tmp
-    curl -OL https://github.com/theowenyoung/$binname/releases/latest/download/$binname-epub-x86_64-unknown-linux-musl.tar.gz
-    tar -xf /tmp/$binname-x86_64-unknown-linux-musl.tar.gz -C $BIN_DIR
+    curl -OL https://github.com/theowenyoung/mdbook-epub/releases/download/v0.4.2103/mdbook-epub-x86_64-unknown-linux-musl.tar.gz
+    tar -xf /tmp/mdbook-epub-x86_64-unknown-linux-musl.tar.gz -C $BIN_DIR
     curl -OL https://github.com/rust-lang/mdBook/releases/download/v0.4.22/mdbook-v0.4.22-x86_64-unknown-linux-musl.tar.gz
-    tar -xf tmp/mdbook-v0.4.22-x86_64-unknown-linux-musl.tar.gz -C $BIN_DIR
+    tar -xf /tmp/mdbook-v0.4.22-x86_64-unknown-linux-musl.tar.gz -C $BIN_DIR
     curl -OL https://github.com/denoland/deno/releases/download/v1.28.2/deno-x86_64-unknown-linux-gnu.zip
-    unzip /tmp/deno-x86_64-unknown-linux-gnu.zip -d $BIN_DIR
+    unzip -o /tmp/deno-x86_64-unknown-linux-gnu.zip -d $BIN_DIR
 
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     # Mac OSX
     cd -- /tmp/
-    curl -OL https://github.com/theowenyoung/$binname/releases/latest/download/$binname-x86_64-apple-darwin.zip
-    unzip -o /tmp/$binname-x86_64-apple-darwin.zip -d $BIN_DIR
-    curl -OL https://github.com/rust-lang/mdBook/releases/download/v0.4.21/mdbook-v0.4.21-x86_64-apple-darwin.tar.gz
+    curl -OL https://github.com/theowenyoung/mdbook-epub/releases/download/v0.4.2103/mdbook-epub-x86_64-apple-darwin.zip
+    unzip -o /tmp/mdbook-epub-x86_64-apple-darwin.zip -d $BIN_DIR
+    curl -OL https://github.com/rust-lang/mdBook/releases/download/v0.4.22/mdbook-v0.4.22-x86_64-apple-darwin.tar.gz
     tar -xf /tmp/mdbook-v0.4.21-x86_64-apple-darwin.tar.gz -C $BIN_DIR
     curl -OL https://github.com/denoland/deno/releases/download/v1.28.2/deno-x86_64-apple-darwin.zip
     unzip -o /tmp/deno-x86_64-apple-darwin.zip -d $BIN_DIR
@@ -31,4 +32,4 @@ chmod +x $BIN_DIR/*
 
 echo Install Success.
 
-echo Run \`make buildbook\` to build to book-dist folder
+echo Run \`make build\` to build
